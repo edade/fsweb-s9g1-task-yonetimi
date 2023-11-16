2c22677dc1834f88149bdbe4dd456b8ea9267573
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-11-16 21:28:32 +0300

    checkbox textarea validasyon, reset, isvalid eklnd

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-export default function TaskHookForm() {
-  const {register, handleSubmit, formState:{errors}}=useForm({defaultValues: {title:"", description:"", people:[]}})
+import { nanoid } from "nanoid";
+export default function TaskHookForm({kisiler, submitFn}) {
+  const {register, handleSubmit, reset,  formState:{errors, isValid}}=useForm({defaultValues: {title:"", description:"", people:[]}, mode:"onChange"})
 
-
-
-  const onSubmit = (formData) =>{
-    console.log(formData)
-  } 
+  function onSubmit(e) {
+    console.log("form çalıştı")
+    submitFn({
+      ...e,
+      id: nanoid(5),
+      status: "yapılacak",
+    });
+    reset();
+  }
   return (
 
 <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
@@ -28,24 +33,36 @@ export default function TaskHookForm() {
   <textarea
     className="input-textarea"
     id="description"
-    {...register("description")}
+    {...register("description", {required:"Task açıklaması yazmalısınız", minLength: { value: 10, message: "Task açıklaması en az 10 karakter olmalı" }})}
   ></textarea>
+  <div className='input-error'>{errors?.description?.message}</div>
 </div>
 
 <div className="form-line">
-  <label className="input-label">İnsanlar</label>
-  <div>
-      <label className="input-checkbox">
-        <input
-          type="checkbox"
-        />
-      </label>
-  </div>
-</div>
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox" 
+                {...register("people", {
+                  required:"Lütfen en az bir kişi seçin",
+                  validate: {maxLimit: (p) => p.length <= 3 || "En fazla 3 kişi seçebilirsiniz", 
+                minLimit: (p)=> p.length >=1 || "Lütfen en az bir kişi seçin"}
+                  })}
+                value={p}
+              />
+              {p}
+            </label>
+          ))}
+        </div>
+        <div className='input-error'>{errors?.people?.message}</div>
+      </div>
 <div className="form-line">
   <button
     className="submit-button"
-    type="submit">
+    type="submit"
+    disabled= {!isValid}>
     Kaydet
   </button>
 </div>
