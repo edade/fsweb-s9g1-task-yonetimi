12ae5951f1e6f8f276558d2e8197e76c876726a1
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-11-16 20:15:47 +0300

    hook-form yapısı oluşturuldu

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,54 @@
 import React from 'react'
-
+import { useForm } from 'react-hook-form'
 export default function TaskHookForm() {
+  const {register, handleSubmit, formState:{errors}}=useForm({defaultValues: {title:"", description:"", people:[]}})
+
+
+
+  const onSubmit = (formData) =>{
+    console.log(formData)
+  } 
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
+
+<form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
+<div className="form-line">
+  <label className="input-label" htmlFor="title">
+    Başlık
+  </label>
+  <input
+     id="title" className="input-text" type='text' {...register("title", {required:"Task başlığı yazmalısınız", minLength: { value: 3, message: "Task başlığı en az 3 karakter olmalı" }})}
+  />
+  <div className='input-error'>{errors?.title?.message}</div>
+</div>
+
+<div className="form-line">
+  <label className="input-label" htmlFor="description">
+    Açıklama
+  </label>
+  <textarea
+    className="input-textarea"
+    id="description"
+    {...register("description")}
+  ></textarea>
+</div>
+
+<div className="form-line">
+  <label className="input-label">İnsanlar</label>
+  <div>
+      <label className="input-checkbox">
+        <input
+          type="checkbox"
+        />
+      </label>
+  </div>
+</div>
+<div className="form-line">
+  <button
+    className="submit-button"
+    type="submit">
+    Kaydet
+  </button>
+</div>
+</form>
   )
 }
